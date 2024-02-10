95226f096e74f58c1237cdff82ce057ae10d8f14
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-02-10 12:06:11 +0100

    [NEW] Creado componente de pantalla inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import InitialScreen from './components/InitialScreen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div style={styling} className="App">
+    <div style={{width: '100%', height: '100%'}}>
+      <InitialScreen />
     </div>
+  </div>
   );
+}
+
+
+const styling = {
+  justifyContent: "center", 
+  alignItems: "center", 
+  display: "flex", 
+  width: '100%', 
+  height: '40vw'
 }
 
 export default App;
---
+++ src/components/InitialScreen.js
@@ -0,0 +1,9 @@
+function InitialScreen() {
+  return (
+  <div style={{width: '100%', height: '100%'}}>
+    <h1> INITIALSCREEN </h1>
+  </div>
+  );
+}
+
+export default InitialScreen;
